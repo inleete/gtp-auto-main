28b6986be4c93b905106fda54a2274e3fdd9f1a9
GTP_Auto <gtp@auto.com>
2025-06-18 15:26:36 +0000

    feat: integrate all modules into unified strategy main executor

--- gtp_auto/gtp_auto/strategy/main.py
+++ gtp_auto/gtp_auto/strategy/main.py
@@ -1,41 +1,21 @@
-import datetime
-from gtp_auto.config.time import is_trade_window_now
-
-def get_kst_time():
-    return datetime.datetime.utcnow() + datetime.timedelta(hours=9)
-
-def is_trade_time():
-    kst_now = get_kst_time()
-    hour = kst_now.hour
-    minute = kst_now.minute
-
-    allowed_time_ranges = [
-        (7, 50, 8, 10),
-        (22, 50, 23, 10),
-    ]
-
-    for start_h, start_m, end_h, end_m in allowed_time_ranges:
-        start = datetime.time(hour=start_h, minute=start_m)
-        end = datetime.time(hour=end_h, minute=end_m)
-        now = kst_now.time()
-        if start <= now <= end:
-            return True
-    return False
+from gtp_auto.config.time import is_trade_time
+from gtp_auto.analysis.signal import evaluate_price_signal
+from gtp_auto.execution.entry import run_entry
+from gtp_auto.utils.logger import log
 
 def main():
-    print("")
-    now = get_kst_time()
-    print(f"KST : {now.strftime('%Y-%m-%d %H:%M:%S')}")
+    log("   ")
 
-    if not is_trade_window_now():
-        print("[GTP]     .")
+    if not is_trade_time():
+        log("      -  ")
         return
 
-    if is_trade_time():
-        print("   ")
-    else:
-        print("   ")
+    log("    ")
+
+    signal = evaluate_price_signal()
+    log(f"   : {signal}")
+
+    run_entry(signal)
 
 if __name__ == "__main__":
     main()
-
